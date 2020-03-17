30799aa0a35fe56d0eb3401f5c3019dc6b8ec7cc
Chris Pond <chris@chrispond.com>
2020-03-16 21:12:57 -0600

    Running some tests to see if I can get the site to build on netlify

--- src/app.js
+++ src/app.js
@@ -16,13 +16,13 @@ app.engine('ejs', require('ejs').__express);
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(methodOverride());
-router.use('/public', express.static('./dist/public'));
+// router.use('/public', express.static('./dist/public'));
 router.use(require('./middleware/prismic'));
 
 // Routes
 router.use(require('./routes/index'));
 router.use(require('./routes/blog-post'));
 
-app.use('/.netlify/functions/app', router);
+app.use('/.netlify/functions/app/', router);
 
 module.exports.handler = serverless(app);
