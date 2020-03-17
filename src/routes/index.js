efabf33747eac2799c13b1badb9243f8d2d34b6e
Chris Pond <chris@chrispond.com>
2020-03-17 17:52:31 -0600

    second swing at directory

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,22 +7,22 @@ const fs = require('fs');
 const path = require("path");
 
 const router = express.Router();
-const currentDir = process.env.LAMBDA_TASK_ROOT;
+// const currentDir = process.env.LAMBDA_TASK_ROOT;
 
 const homeRouter = router.get('/index', (request, response) => {
 	request.prismic.api.getByUID('homepage', 'home').then(homeResponse => {
 		request.prismic.api
 		.query(Prismic.Predicates.at("document.type", "blog_post"))
 		.then(blogResponse => {
-			fs.readdir(currentDir, function(err, items) {
+			fs.readdir(__dirname, function(err, items) {
 				console.log('****************************', items);
 			 
 				response.json({items: items, error: err});
 			});
 
-			// fs.readFile(path.join(`${currentDir}/views/index.ejs`), 'utf8', function(err, data) {
+			// fs.readFile(path.join(`${__dirname}/src/views/index.ejs`), 'utf8', function(err, data) {
 			// 	if (err) {
-			// 	  response.json({dir: currentDir, error: err});
+			// 	  response.json({dir: __dirname, error: err});
 			// 	} else {
 			// 	  response.send(ejs.render(data, {global: homeResponse.data, blogPosts: blogResponse.results, PrismicDOM}));
 			// 	}
