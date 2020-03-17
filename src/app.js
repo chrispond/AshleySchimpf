cc96a478cb3ab20c3694fa3ed2ce795a6cffef9d
Chris Pond <chris@chrispond.com>
2020-03-16 21:04:21 -0600

    Cleaned up some files. Fixed some build commands. Setup static files route

--- src/app.js
+++ src/app.js
@@ -5,28 +5,24 @@ const methodOverride = require("method-override");
 const path = require("path");
 const serverless = require('serverless-http');
 
-const Prismic = require('prismic-javascript');
-
 const app = express();
 const router = express.Router();
 
-// Settings
+// Template Settings
 app.set("views", path.join("./src/views"));
 app.engine('ejs', require('ejs').__express);
-
-app.use(express.static(path.join("./dist/public")));
-// app.use(express.static(__dirname + '../public'));
 
 // Middleware
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(methodOverride());
+router.use('/public', express.static('./dist/public'));
 router.use(require('./middleware/prismic'));
 
 // Routes
 router.use(require('./routes/index'));
 router.use(require('./routes/blog-post'));
 
-app.use('/app', router);
+app.use('/.netlify/functions/app', router);
 
 module.exports.handler = serverless(app);
