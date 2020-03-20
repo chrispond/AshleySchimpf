f42d2c063ed8f2f721323a0045cc945c13c4b7bf
Chris Pond <chris@chrispond.com>
2020-03-20 17:11:16 -0600

    cleanup dependancies, ignore files etc. added redirect

--- src/views/views.js
+++
@@ -1,9 +0,0 @@
-let fs = require("fs");
-const rContents = require(`${__dirname}/views/index.ejs`);
-
-export async function handler(event, context) {
-    return {
-        statusCode: 200,
-        body: rContents
-    };
-}
