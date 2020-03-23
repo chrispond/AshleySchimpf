250100022d996272c2d1186ff3564a52c8a2cd35
Chris Pond <chris@chrispond.com>
2020-03-22 23:35:44 -0600

    Test for community support

--- src/app.js
+++ src/app.js
@@ -1,25 +1,9 @@
 // Dependancies
-const bodyParser = require("body-parser");
 const express = require('express');
-const methodOverride = require("method-override");
-const path = require("path");
 const serverless = require('serverless-http');
 
 const app = express();
 const router = express.Router();
-
-// Template Settings
-// app.set('view engine', 'ejs');
-
-// // Middleware
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(methodOverride());
-// app.use(require('./middleware/prismic'));
-
-// Routes
-// router.use(require('./routes/index'));
-// router.use(require('./routes/blog-post'));
 
 router.get('/', (request, response) => {
     response.json({"test": "test"});
